9c029eff43f61cc4fbbacbb47e1a2c8715ef7ddd
TasakiDaichi <d.tasaki1212@icloud.com>
2025-10-05 18:35:38 +0900

    profileデザインまで

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -5,25 +5,34 @@ import Typography from '@mui/material/Typography';
 import { CardActions } from '@mui/material';
 import Button from '@mui/material/Button';
 
-import type { Project } from './types';
 import projectData from './projectData';
 
 const Project: React.FC = () => {
     return (
         <>
-            {projectData.map((p: Project) => {
+            {projectData.map(p => (
                 <Card key={p.id} style={{ marginBottom: 12 }}>
                     <CardContent>
                         <Typography variant="h6">{p.name}</Typography>
                         <Typography color="textSecondary">{p.description}</Typography>
                     </CardContent>
                     <CardActions>
-                        {p.link  ? <Button href={p.link} variant="text" size="small" color="primary" target="_blank">プロジェクトを見る</Button> : null}
-                        {p.movieLink  ? <Button href={p.movieLink} variant="text" size="small" color="primary" target="_blank">動画を見る</Button> : null}
-                        {!p.link && !p.movieLink  ? <Typography variant="body2">現在制作中...</Typography> : null}
+                        {p.link ? (
+                            <Button href={p.link} variant="text" size="small" color="primary" target="_blank" rel="noopener noreferrer">
+                                プロジェクトを見る
+                            </Button>
+                        ) : null}
+
+                        {p.movieLink ? (
+                            <Button href={p.movieLink} variant="text" size="small" color="primary" target="_blank" rel="noopener noreferrer">
+                                動画を見る
+                            </Button>
+                        ) : null}
+
+                        {!p.link && !p.movieLink ? <Typography variant="body2">現在制作中...</Typography> : null}
                     </CardActions>
                 </Card>
-            })}
+            ))}
         </>
     )
 }
